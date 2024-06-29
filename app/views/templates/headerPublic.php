fccf0be7c93ebeac32e31dd0534e63362b7e6a80
Kalen <116669445+kaleper@users.noreply.github.com>
2024-06-29 19:43:33 +0000

    Create header template for view
    Template for authenticated users. Added in bootstrap navbar code.

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -8,7 +8,8 @@ if (isset($_SESSION['auth']) == 1) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<head>
+    <html lang="en">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Sign In</title>
     <meta charset="UTF-8">
