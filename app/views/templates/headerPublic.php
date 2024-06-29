4ef2c650f2d71590237709eda3b50a728fe9cf50
Kalen <116669445+kaleper@users.noreply.github.com>
2024-06-29 19:36:21 +0000

    Create headerPublic template for view
    Contains boilerplate code to be displayed when user is not authenticated

---
+++ app/views/templates/headerPublic.php
@@ -0,0 +1,20 @@
+<?php
+/* Header to be displayed when user is not logged in */
+
+// If user is logged in, redirect to home page automatically
+if (isset($_SESSION['auth']) == 1) {
+    header('Location: /home');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Sign In</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+</head>
+    
+<body>
